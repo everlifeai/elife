a16f053da4aca181af1d58321d06ba7715467da9
Productive Programmer <charles.lobo@gmail.com>
2018-09-23 21:57:48 +1000

    Support multiple nodes on same machine

--- run.sh
+++ run.sh
@@ -14,8 +14,6 @@ $SCRIPTNAME <command>
 EOF
 }
 
-NAME=elife
-
 #       problem/
 # Some of our repositories are privately hosted on github. These are not
 # accesible within the docker container.
@@ -36,19 +34,54 @@ function setup() {
         everlifeai/elife yarn install
 }
 
+#       problem/
+# Our node runs within a docker container and this is a 'virtual'
+# machine with it's own IP address. However this IP address is not on
+# our 'normal' network (and we don't want to set up a docker network)
+#
+#       way/
+# We carry across our 'host' IP address into the container as an
+# environment variable so it is accessible within the node. We also
+# carry across port numbers so we can run multiple nodes without
+# conflicting on the same machine (we update the name for the same
+# reason)
+#
+function setupDockerParams() {
+    EVERLIFE_HOST_IP=$(ifconfig | grep 'inet '| grep -v 127.0.0.1 | awk '{print $2}')
+    if [ -z "$EVERLIFE_HOST_PORT" ]
+    then
+        NAME="elife"
+        EVERLIFE_HOST_PORT="8997"
+    else
+        NAME="elife-$EVERLIFE_HOST_PORT"
+    fi
+}
+
 function avatar() {
+    setupDockerParams
+
     docker run -it --rm \
         -v "$(pwd):/code" \
         -v "$HOME/.ssh:/root/.ssh" \
         -w "/code" \
         -e "HOME=/tmp" \
-        -e EVERLIFE_HOST_IP="$(ifconfig | grep 'inet '| grep -v 127.0.0.1 | awk '{print $2}')" \
-        -p "8997:8997" \
+        -e EVERLIFE_HOST_IP="$EVERLIFE_HOST_IP" \
+        -p "$EVERLIFE_HOST_PORT:8997" \
         --name "${NAME}" \
         --env-file cfg.env \
         everlifeai/elife ./run.sh cnt_start_avatar
 }
 
+#       understand/
+# This function is called within the docker container to start the node
+# once the docker virtual machine has started up
+#
+#       outcome/
+# Start the redis server, wait 10 seconds so it has time to start up,
+# then start the node
+#
+# TODO: Start default AI service here as well?
+#
 function cnt_start_avatar() {
     start_redis
     sleep 10
@@ -59,10 +92,18 @@ function start_redis() {
     /root/redis-4.0.11/src/redis-server &
 }
 
+#       outcome/
+# Enter the running docker container to execute tests, manual commands
+# etc etc.
 function enter() {
+    setupDockerParams
+    echo Entering ${NAME}...
     docker exec -it "${NAME}" bash
 }
 
+#       outcome/
+# Run the appropriate function based on the command-line parameter
+# If nothing was run, show the help.
 RUN=0
 function run_fn() {
     if [ "$1" == "$2" ]
